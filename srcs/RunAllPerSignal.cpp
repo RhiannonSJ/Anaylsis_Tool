c4b1d6bb6f0148017569edbe3bddf9b63608b58d
Rhiannon Jones <rjones@hep.ph.liv.ac.uk>
2018-02-15 00:34:44 +0000

    MainCC0Pi runs and produces non-normalised for POT plots of all sorts of types. The functionality works. The sample size needs drastically increasing

--- srcs/RunAllPerSignal.cpp
+++ srcs/RunAllPerSignal.cpp
@@ -37,12 +37,12 @@ namespace ana{
       // Fill the testing histograms
       GetTrueRecoHists( testing_events, topology, h_true_test, h_reco_test );
 
-      // Scaling factor for full POT for 6009 events
+      // Scaling factor for full POT for 3000 events (half of the full available sample
       // For now, averaged normalisations for 3 genie models
-      double scale_POT = 1100;
+      //double scale_POT = 2250;
       
-      h_true_test->Scale(scale_POT);
-      h_reco_test->Scale(scale_POT);
+      //h_true_test->Scale(scale_POT);
+      //h_reco_test->Scale(scale_POT);
       
       // Unfold 
       RooUnfoldBayes unfold( &response, h_reco_test, 1 );
